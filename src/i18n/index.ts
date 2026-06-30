27b855c39c3c6ce155cee7e0f986b3911e934e33
boyce <1266280@qq.com>
2026-06-30 08:45:30 +0800

    fix: inline en-US.json, remove cross-project import

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enUS from '../../../landing-site-astro/src/i18n/locales/en-US.json';
+import enUS from './en-US.json';
 
 i18next.use(initReactI18next).init({
   resources: { 'en-US': { translation: enUS } },
